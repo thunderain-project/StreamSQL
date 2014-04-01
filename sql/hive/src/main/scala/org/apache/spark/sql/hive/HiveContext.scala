e67eb52226908f86bf4e060d734c04c0497aff86
jerryshao <saisai.shao@intel.com>
2014-04-01 16:30:53 +0800

    [StreamSQL] Add missing strategies in HiveContext

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveContext.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveContext.scala
@@ -227,7 +227,8 @@ class HiveContext(sc: SparkContext) extends SQLContext(sc) {
       HashJoin,
       BasicOperators,
       CartesianProduct,
-      BroadcastNestedLoopJoin
+      BroadcastNestedLoopJoin,
+      SparkSpecificStrategies
     )
   }
 
